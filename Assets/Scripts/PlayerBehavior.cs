a1b11799e4f088ace96a80d31f67d17c29f93d6b
Muhamad Fajar <72683008+fajarbinus@users.noreply.github.com>
2026-05-21 10:52:14 +0700

    Add Inter fonts, Mo material & lighting
    Add Inter font assets (SDF atlases, TTFs and meta files), a new Mo material (+meta), New Lighting Settings and .vsconfig. Rename SampleScene3DOnly.unity to Main.unity and add Prefabs.meta. Also update PlayerBehavior.cs, PlayerMaterialWhite.mat, TextMesh Pro fallback asset and ProjectSettings/TagManager.asset to reflect scene/asset changes.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -61,7 +61,7 @@ public class PlayerBehavior : MonoBehaviour
             Vector3 spawnPos = transform.position + transform.forward *1f;
             GameObject newBullet = Instantiate(Bullet, spawnPos, this.transform.rotation);
             Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
-            bulletRB.velocity = transform.forward * BulletSpeed;
+            bulletRB.linearVelocity = transform.forward * BulletSpeed;
             _isShooting = false;
         }
             
